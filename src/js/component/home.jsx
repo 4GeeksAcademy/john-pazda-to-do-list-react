bb1342d8dfe019fdba83b2414b018f8d4117cda3
Jpazda <111332675+Jpazda@users.noreply.github.com>
2023-09-01 23:25:43 +0000

    First Commit

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,25 +1,59 @@
-import React from "react";
+import React, { useState } from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
+	const [ inputValue, setInputValue ] = useState("");
+	const [ tasks, setTasks] = useState([]);
+
+
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="container d-flex flex-column m-5 align-items-center">
+			<h1 className="text-primary">Tasks: {inputValue}</h1>
+			<div className="container d-flex flex-row justify-content-center m-2">	
+				<span className="input-group-text" id="basic-addon1">To do tasks:</span>
+				<input 
+				type="text" 
+				className="form-control w-50" 
+				placeholder="Add your tasks here" 
+				aria-label="Username" 
+				aria-describedby="basic-addon1" 
+				onChange={(e) => setInputValue(e.target.value)}
+				onKeyUp={(e) => {
+					if (e.key === "Enter"){
+						setTasks(tasks.concat(inputValue));
+						setInputValue('');
+					} 
+					
+				}}
+				value={inputValue}
+				/>	
+			</div>		
+			<button 
+			className="btn btn-primary btn-md mt-3 mb-3"
+			onClick={(e) => {
+				setTasks(tasks.concat(inputValue));
+				setInputValue('');
+			}}
+			>
+				Add to list:
+			</button>
+				<ul className="list-group border border-primary border-3 rounded-1 w-25 d-flex flex-column align-items-center">
+					{tasks.map((task,index) => (
+						<li className="list-group-item w-100 d-flex justify-content-between">
+							{task} <i 
+							className="fa-solid fa-trash"
+							onClick={() => setTasks(tasks.filter((t, currentIndex) => index != currentIndex))}
+							></i>
+						</li>
+
+					))}
+				</ul>
+			
 		</div>
+		
 	);
 };
 
